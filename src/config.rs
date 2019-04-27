b2c3a1f8fbb7cce25ff3f17db258b1a4ced528a0
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-04-27 13:43:46 +0600

    Complete the description of the --tor-proxy (config.rs)

--- src/config.rs
+++ src/config.rs
@@ -115,6 +115,9 @@ pub struct SocketConfig {
 
     /// Connect all future sockets to a local tor proxy, specified as an IP
     /// address or a domain name and a port, separated by a colon.
+    ///
+    /// Typically, a tor proxy runs on 127.0.0.1:9050. You can edit its
+    /// configuration located in `/etc/tor/torrc`.
     #[structopt(long = "tor-proxy", takes_value = true, value_name = "SOCKET-ADDRESS")]
     pub tor_proxy: Option<SocketAddr>,
 }
